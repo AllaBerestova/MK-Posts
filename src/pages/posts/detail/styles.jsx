7503c6090c5c3d21bab05cb812b44721ded30c15
Alla Berestova <alla.chigvintseva28@gmail.com>
2025-06-23 14:42:30 +0300

    add delete post

--- src/pages/posts/detail/styles.jsx
+++ src/pages/posts/detail/styles.jsx
@@ -9,10 +9,60 @@ export const Text = styled.div`
   font-size: 15px;
 `;
 export const LinkWrapper = styled.div`
+  align-items: center;
   margin: 15px 0 0 0;
   display: flex;
   gap: 15px;
   width: 100%;
   justify-content: center;
 `;
-
+export const DeleteButton = styled.button`
+  border: 1px solid black;
+  border-radius: 10px;
+  background: white;
+  padding: 5px 15px;
+  color: black;
+  cursor: pointer;
+  &:hover {
+    background: #7a0c0c;
+    color: white;
+    border: 1px solid #7a0c0c;
+  }
+`;
+export const ModalWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+`;
+export const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  color: black;
+  border: 1px solid white;
+  border-radius: 15px;
+  padding: 25px;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+export const ModalText = styled.div`
+  color: black;
+  font-weight: bold;
+  font-size: 20px;
+  text-align: center;
+`
+export const ModalContent = styled.div`
+  display: flex;
+  gap: 15px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
